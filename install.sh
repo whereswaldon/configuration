676d295ad760f4cc6139e799de007b421313fc9c
Chris Waldon <Chris.Waldon@ibm.com>
2018-10-12 12:57:13 -0400

    Auto-install kak-connect

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ symlink "$HOME/.local/bin/dwm.sh" "$BASEDIR/bin/dwm.sh"
 symlink "$HOME/.local/bin/get-layout" "$BASEDIR/bin/get-layout"
 symlink "$HOME/.local/bin/reconfigure-monitors" "$BASEDIR/bin/reconfigure-monitors"
 symlink "$HOME/.local/bin/sas-vpn" "$BASEDIR/bin/sas-vpn"
+symlink "$HOME/.local/bin/kak-connect" "$BASEDIR/kak/plugins/kak-connect/bin/kak-connect"
 
 # special case
 if [ -e "$BASEDIR/kak/autoload/system" ] ; then rm -v "$BASEDIR/kak/autoload/system" ; fi
